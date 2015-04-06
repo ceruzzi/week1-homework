808e51f048f2385c83f2381d6b3019f20589f7eb
Michael Ceruzzi <ceruzzi@uchicago.edu>
2015-04-05 21:55:02 -0500

    ok
    ok

--- 1_planets.rb
+++ 1_planets.rb
@@ -19,3 +19,9 @@ planets = ['Mercury', 'Venus', 'Earth', 'Mars', 'Saturn', 'Jupiter', 'Uranus', '
 #
 # Your code goes here:
 #
+
+i =1;
+planets.each do |word|
+  puts "Planet #{i}: #{word}"
+  i+=1
+end
